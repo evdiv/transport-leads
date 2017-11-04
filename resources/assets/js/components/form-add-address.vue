f7a647baa14c6455f204815069630e61ffc13c6a
Eugene Dvoir <edvoir@mail.com>
2017-11-03 21:46:21 -0400

    Add Submit Address block

--- resources/assets/js/components/form-add-address.vue
+++ resources/assets/js/components/form-add-address.vue
@@ -9,15 +9,23 @@
 
                 <div class="col-md-6">
                     <input type="text" class="form-control" name="loading-location" value="" required autofocus>
-
                 </div>
+            </div>
 
+            <div v-for="(location, index) in locations">
+                <div class="form-group">
+                    <label for="additional-location" class="col-md-4 control-label">Unloading Point</label>
+
+                    <div class="col-md-6">
+                        <input type="text" class="form-control" name="additional-location[]" value="" required autofocus>
+                    </div>
+                </div>
             </div>
 
 
             <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary">
+                    <button class="btn btn-primary" @click="addLocation">
                       Add Destination or Additional point
                     </button>
                 </div>
@@ -25,4 +33,21 @@
         </form>
     </div>
 </div>
+
 </template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                locations: 0,
+            }
+        },
+
+        methods: {
+           addLocation: function () {
+                this.locations++;
+            }
+        }
+    }    
+</script>   
